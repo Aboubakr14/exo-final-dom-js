83741833d903a73cfa7a081629c1a0194e24e436
Benali Abou bakr <adametkawtar@gmail.com>
2020-01-28 13:09:40 +0100

    fin du premier exercice

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById("titr").textContent = 'Mon titre modifié';
+document.getElementById("titr").style.color = 'blue';
+
+let titre = document.getElementById("titr");
+
+titre.classList.add("bg-dark")
+
+
