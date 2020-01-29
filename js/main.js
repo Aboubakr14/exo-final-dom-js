7e2a288572b10f841dc23b2fa8faeed32fd69395
Benali Abou bakr <adametkawtar@gmail.com>
2020-01-29 09:03:59 +0100

    fin du troisième exerice

--- js/main.js
+++ js/main.js
@@ -17,5 +17,29 @@ titre.classList.add("bg-dark")
 // exercice 2 //
 change1.addEventListener("click", () =>  document.getElementById("exercice2").style.display = "none")
 change1.addEventListener("click", () =>  document.getElementById("marg").style.display = "block")
+change1.addEventListener("click", () =>  document.getElementById("exercice3").style.display = "none")
 change2.addEventListener("click", () =>  document.getElementById("marg").style.display = "none")
+change2.addEventListener("click", () =>  document.getElementById("exercice3").style.display = "none")
 change2.addEventListener("click", () =>  document.getElementById("exercice2").style.display = "block")
+change3.addEventListener("click", () =>  document.getElementById("marg").style.display = "none")
+change3.addEventListener("click", () =>  document.getElementById("exercice2").style.display = "none")
+change3.addEventListener("click", () =>  document.getElementById("exercice3").style.display = "block")
+// exercice 3 //
+let nom = document.getElementById("name");
+let title = document.getElementById("title");
+let bouton = document.getElementById("bouton");
+
+let go = (e) => {
+    title.innerHTML = nom.value;
+    nom.value = "";
+}
+
+let goes = (e) => {
+    if (e.keyCode === 13) {
+        title.innerHTML = nom.value;
+        nom.value = "";
+    }
+}
+
+bouton.addEventListener("click", go);  
+nom.addEventListener("keypress", goes)
